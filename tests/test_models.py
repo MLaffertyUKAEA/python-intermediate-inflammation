e72769fede23fd9fdfc47625067bda7d6cd92829
Mark Lafferty <mark.lafferty@ukaea.uk>
2023-11-10 15:31:02 +0000

    Add parameterized mean, min and max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,32 +3,21 @@
 import numpy as np
 import numpy.testing as npt
 import os
+import pytest
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
 
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
@@ -38,27 +27,35 @@ def test_load_from_json(tmpdir):
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
 
-def test_daily_min():
+@pytest.mark.parametrize(
+        "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([ [2, 3], [4, 5], [6, 1]], [2, 1]),
+        ([ [-1, 1], [-2, 2], [-3, 3]], [-3, 1])
+    ]
+)
+
+def test_daily_min(test, expected):
     """Test that daily_min works"""
     from inflammation.models import daily_min
 
-    test_input = np.array([[-3, 3],
-                          [-2, 2],
-                          [0, 0]])
-    test_result = np.array([-3, 0])
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-    npt.assert_array_equal(daily_min(test_input), test_result)
+@pytest.mark.parametrize(
+        "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([ [2, 3], [4, 5], [6, 1]], [6, 5]),
+        ([ [-1, 1], [-2, 2], [-3, 3]], [-1, 3])
+    ]
+)
 
-def test_daily_max():
+def test_daily_max(test, expected):
     """Test that daily_max works"""
     from inflammation.models import daily_max
 
-    test_input = np.array([[-3, 3],
-                          [-2, 2],
-                          [0, 0]])
-    test_result = np.array([0, 3])
-
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
