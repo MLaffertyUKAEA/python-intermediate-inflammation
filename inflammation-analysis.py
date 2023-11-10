b9eec6764cb7908fe0212bf6fd0132acfb42fbed
Mark Lafferty <mark.lafferty@ukaea.uk>
2023-11-10 13:36:01 +0000

    Further style fixes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -10,12 +10,15 @@ from inflammation.compute_data import analyse_data
 
 
 def main(args):
+
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
+
+
     in_files = args.in_files
     if not isinstance(in_files, list):
         in_files = [args.in_files]
@@ -26,6 +29,7 @@ def main(args):
         return
 
     for filename in in_files:
+
         inflammation_data = models.load_csv(filename)
 
         view_data = {'average': models.daily_mean(inflammation_data),
@@ -34,7 +38,9 @@ def main(args):
 
         views.visualize(view_data)
 
+
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(
         description='A basic patient inflammation data management system')
 
